cac27af538a98e093acb60a7fe7c3d4e78bf2ebf
Funatiker1968 <y.kaya@web.de>
2025-12-14 03:30:40 +0100

    FIX: Alle kritischen Fehler behoben - App läuft jetzt
    - Keine Analyzer Errors mehr
    - Home-Screen Navigation funktioniert
    - Import mit Fortschrittsbalken und Terminal
    - Eurojackpot Import aus TXT implementiert
    - UI Kontraste verbessert

--- lib/screens/intro_slot_screen.dart
+++ lib/screens/intro_slot_screen.dart
@@ -2,54 +2,104 @@ import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-class IntroSlotScreen extends StatefulWidget {
-  const IntroSlotScreen({super.key});
+class SlotIntroScreen extends StatefulWidget {
+  const SlotIntroScreen({super.key});
 
   @override
-  State<IntroSlotScreen> createState() => _IntroSlotScreenState();
+  State<SlotIntroScreen> createState() => _SlotIntroScreenState();
 }
 
-class _IntroSlotScreenState extends State<IntroSlotScreen> {
-  final List<String> chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split('');
-  final Random rnd = Random();
+class _SlotIntroScreenState extends State<SlotIntroScreen> {
+  final List<String> chars = [
+    'A','B','C','D','E','F','G','H','I','J',
+    'K','L','M','N','O','P','Q','R','S','T',
+    'U','V','W','X','Y','Z','0','1','2','3',
+    '4','5','6','7','8','9'
+  ];
 
   late Timer _timer;
-  List<String> reel = List.filled(18, '?');
+  List<String> row1 = [];
+  List<String> row2 = [];
+  List<String> row3 = [];
+  int tick = 0;
 
-  bool _finished = false;
-  int _ticks = 0;
-
-  final String finalText =
-      "LOTTOZAHLENGENERATOR FÜR LOTTO 6AUS49 UND EUROJACKPOT BY FUNATIKER";
+  bool finished = false;
 
   @override
   void initState() {
     super.initState();
+    _startAnimation();
+  }
 
-    _timer = Timer.periodic(const Duration(milliseconds: 80), (_) {
+  void _startAnimation() {
+    final rnd = Random();
+    row1 = List.generate(16, (_) => chars[rnd.nextInt(chars.length)]);
+    row2 = List.generate(16, (_) => chars[rnd.nextInt(chars.length)]);
+    row3 = List.generate(16, (_) => chars[rnd.nextInt(chars.length)]);
+
+    _timer = Timer.periodic(const Duration(milliseconds: 60), (_) {
       setState(() {
-        _ticks++;
+        tick++;
 
-        if (_ticks < 40) {
-          // Phase 1: völliger Buchstabensalat
-          reel = List.generate(18, (_) => chars[rnd.nextInt(chars.length)]);
-        } else if (_ticks < 100) {
-          // Phase 2: Reel langsam stabilisieren
-          for (int i = 0; i < reel.length; i++) {
-            if (rnd.nextDouble() < (_ticks - 40) / 60) {
-              reel[i] = finalText[i];
-            } else {
-              reel[i] = chars[rnd.nextInt(chars.length)];
-            }
-          }
-        } else {
-          // Phase 3: Finaltext steht komplett
-          reel = finalText.substring(0, 18).split('');
-          _finished = true;
-          _timer.cancel();
+        if (tick <= 40) {
+          row1.shuffle();
+        }
+        if (tick <= 70) {
+          row2.shuffle();
+        }
+        if (tick <= 100) {
+          row3.shuffle();
+        }
+
+        if (tick == 110) {
+          _showFinalMessage();
         }
       });
     });
+  }
+
+  void _showFinalMessage() {
+    setState(() {
+      finished = true;
+      row1 = "LOTT OGEN ERAT OR".split('');
+      row2 = "FÜR 6AUS49".padRight(16).split('');
+      row3 = "UND EUROJACKPOT".padRight(16).split('');
+    });
+    _timer.cancel();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) Navigator.of(context).pushReplacementNamed('/home');
+    });
+  }
+
+  Widget _buildRow(List<String> chars) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: chars.map((c) {
+          return Container(
+            width: 20,
+            height: 28,
+            margin: const EdgeInsets.symmetric(horizontal: 1),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(4),
+              border: Border.all(color: Colors.white24, width: 1),
+            ),
+            child: Text(
+              c,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.greenAccent,
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
   }
 
   @override
@@ -60,76 +110,24 @@ class _IntroSlotScreenState extends State<IntroSlotScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Walzenfeld
-            Container(
-              padding: const EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(color: Colors.white, width: 3),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: reel
-                    .map(
-                      (c) => Container(
-                        width: 20,
-                        height: 40,
-                        alignment: Alignment.center,
-                        margin: const EdgeInsets.symmetric(horizontal: 2),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade900,
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        child: Text(
-                          c,
-                          style: theme.textTheme.titleLarge?.copyWith(
-                            color: Colors.yellow,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    )
-                    .toList(),
-              ),
-            ),
-            const SizedBox(height: 40),
-
-            if (_finished)
-              Column(
-                children: [
-                  Text(
-                    "Lottozahlengenerator",
-                    style: theme.textTheme.headlineSmall?.copyWith(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "für Lotto 6aus49 und Eurojackpot",
-                    style: theme.textTheme.titleMedium?.copyWith(
-                      color: Colors.yellow,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "by Funatiker",
-                    style: theme.textTheme.titleMedium?.copyWith(
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+            _buildRow(row1),
+            _buildRow(row2),
+            _buildRow(row3),
+            const SizedBox(height: 20),
+            if (finished)
+              const Text(
+                "by Funatiker",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.yellowAccent,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
           ],
         ),
